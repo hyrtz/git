9b46bf32ecba72332fc5f9800e09d087e9c0e297
Mira Desaville <miradeldesaville@gmail.com>
2025-02-13 10:37:01 +0800

    Version 1 restored

--- change.js
+++ change.js
@@ -1,2 +1,1 @@
-
 // add this file to the prev commit 
---
+++ console.js
@@ -0,0 +1,9 @@
+console.log('config.js');
+console.log('version1');
+
+// git status -> tell what changes you made from prev status
+// git add (folder_name) -> pick which changes we want in our next version
+                            // ''.' -> add all files 
+// git commit -m -> creates new version; version == commit 
+            // '-m' -> attaching msg to the commit 
+// git log -> to see version history 
